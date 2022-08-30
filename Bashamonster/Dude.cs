4cc79cc54d3af52ae94f357c72999cafd9b208bb
Castor Dunkels <castordnk@gmail.com>
2022-08-30 13:21:13 +0200

    ._.

---
+++ Bashamonster/Dude.cs
@@ -0,0 +1,8 @@
+public class Dude
+{
+
+    public string Name = "Jeff";
+    public int Hp = 100000000;
+
+        
+}
--- Bashamonster/Program.cs
+++ Bashamonster/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+
+Dude D1 = new Dude();
+Console.WriteLine(D1.Name);
+
+
+while(D1.Hp >= 1){
+    Console.WriteLine(D1.Hp);
+    D1.Hp --;
+}
+
+
+
+Console.ReadLine();
